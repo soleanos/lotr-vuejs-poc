636529d2ae0b20f747bfd572683ab6329e41b0cc
Anthony Archambeau <no@mail.com>
2020-02-21 12:02:14 +0100

    Appel basique a lapi lotr

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,56 @@
+<template>
+  <div class="hello">
+    <div
+      v-for="(item, index) in characters"
+      v-bind:item="item"
+      v-bind:index="index"
+      v-bind:key="item.id">
+      {{item.name}}
+    </div>
+
+  </div>
+</template>
+
+<script>
+import axios from 'axios';
+
+export default {
+  data() {
+    return {
+      characters: [],
+      errors: []
+    }
+  },
+
+  // Fetches posts when the component is created.
+  created() {
+    const instance = axios.create({
+      baseURL: 'https://the-one-api.herokuapp.com/v1',
+      timeout: 1000,
+      headers: {'Authorization': 'Bearer R93Stw7XCYmHuDZgHp9I'}
+    });
+
+    instance.get('/character').then(response => {
+     this.characters = response.data.docs
+    })
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h3 {
+  margin: 40px 0 0;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
